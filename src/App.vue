e9ef9eb74739d9b38a82a25228ca7f7cdf3ccd41
hinatha <hinatha0110@gmail.com>
2022-11-29 16:33:28 +0900

    Component表示完了

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- Component of TodoList -->
+  <TodoList />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script lang="ts">
+import TodoList from './components/TodoList.vue'
+
+export default {
+  // Appears as <App>
+  // FYI: https://prograshi.com/language/vue-js/purpose-of-name-in-export-default/
+  name: 'App', 
+  components: {
+    TodoList // Set TodoList as components
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ul class="todolist">
+      <!-- Display state.todoList with todo as key -->
+      <li v-for="todo in state.todoList" :key="todo.todo">{{ todo.todo }}</li>
+    </ul>
+    <!-- TodoList component has TodoInput component inside it -->
+    <Todo-input />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive} from 'vue'
+import TodoInput from '../components/TodoInput.vue'
+
+
+export default defineComponent({
+  // Define component as TodoList
+  name: 'TodoList',
+  // TodoList component has TodoInput component inside it
+  components: {
+    TodoInput
+  },
+
+  setup() {
+    // In case of lang="ts", define type inside reactive
+    const state = reactive<{ todoList: [{todo: String}, {todo: String}];}>({
+      // There are two temporary data
+      todoList: [
+          {
+            todo: 'todo1',
+          },
+          {
+            todo: 'todo2',
+          }
+
+      ]
+    });
+    return { state };
+  }
+});
+
+</script>
